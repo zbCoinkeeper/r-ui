ad447a5f11514c85c5a472bd43fc64d1e378a0cc
zb <2479107942@qq.com>
2019-12-19 21:46:17 +0800

    first commit

---
+++ lib/index.tsx
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import Icon from './icon';
+
+
+ReactDOM.render(<div>
+    <Icon name="wechat"/>
+  </div>, document.getElementById("root"));
+
+  
